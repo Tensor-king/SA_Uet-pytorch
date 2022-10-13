2ae68077deed073c39952488a28e2aad06b1f1c8
Theo <zyf1787594682@gmail.com>
2022-10-13 17:08:40 +0800

    Update disturtd_utils.py

--- train_utils/disturtd_utils.py
+++ train_utils/disturtd_utils.py
@@ -3,9 +3,10 @@ import torch
 
 class ConfusionMatrix(object):
     def __init__(self, num_classes):
-        self.num_classes = num_classes + 1
+        self.num_classes = num_classes
         self.mat = None
 
+    # 建立左True，右上的混淆矩阵
     def update(self, a, b):
         n = self.num_classes
         if self.mat is None:
@@ -14,7 +15,6 @@ class ConfusionMatrix(object):
         with torch.no_grad():
             # 寻找GT中为目标的像素索引 忽略255(FOV外的像素值)
             k = (a >= 0) & (a < n)
-            # 统计像素真实类别a[k]被预测成类别b[k]的个数(这里的做法很巧妙)
             inds = n * a[k].to(torch.int64) + b[k]
             # 左为True,上为predict
             self.mat += torch.bincount(inds, minlength=n ** 2).reshape(n, n)
@@ -27,14 +27,15 @@ class ConfusionMatrix(object):
         h = self.mat.float()
         # 计算全局预测准确率(混淆矩阵的对角线为预测正确的个数)
         acc_global = (torch.diag(h).sum() / h.sum()).item()
-        # 计算sensitivity
+        # 计算sensitivity(recall)
         se = ((torch.diag(h) / h.sum(1))[1]).item()
-        # 计算specificity(recall)
+        # 计算specificity
         sp = ((torch.diag(h) / h.sum(1))[0]).item()
         # 计算precision
         pr = ((torch.diag(h) / h.sum(0))[1]).item()
         # 计算每个类别的F1-score组成的列表 从0开始
-        F1 = 2 * (pr * sp) / (pr + sp)
+        F1 = 2 * (pr * se) / (pr + se)
+
         # # 计算类别预测与真实目标的iou
         # iu = torch.diag(h) / (h.sum(1) + h.sum(0) - torch.diag(h))
 
