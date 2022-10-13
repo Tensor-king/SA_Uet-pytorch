4bbd94c8e0b15a71e718fd73070e4d799f9f872b
Theo <zyf1787594682@gmail.com>
2022-10-13 17:10:00 +0800

    Update dice_cofficient_loss.py

--- train_utils/dice_cofficient_loss.py
+++ train_utils/dice_cofficient_loss.py
@@ -8,11 +8,35 @@ import torch
 def dice_coeff(x: torch.Tensor, target: torch.Tensor, epsilon=1e-6):
     # Average of Dice coefficient for all batches, or for a single mask
     # 计算一个batch中所有图片某个类别的dice_coefficient
+    # 注意这个函数默认x的通道为1
+    assert x.shape[1] == 1, f"output的通道数不对"
     d = 0.
     batch_size = x.shape[0]
     for i in range(batch_size):
         x_i = x[i].reshape(-1)
         t_i = target[i].reshape(-1).float()
+        inter = torch.dot(x_i, t_i)
+        sets_sum = torch.sum(x_i) + torch.sum(t_i)
+        if sets_sum == 0:
+            sets_sum = 2 * inter
+        d += (2 * inter + epsilon) / (sets_sum + epsilon)
+    return d / batch_size
+
+
+def dice_coeff_mask(x: torch.Tensor, target: torch.Tensor, epsilon=1e-6):
+    # Average of Dice coefficient for all batches, or for a single mask
+    # 计算一个batch中所有图片某个类别的dice_coefficient
+    # 注意这个函数默认x的通道为1
+    assert x.shape[1] == 1, f"output的通道数不对"
+    d = 0.
+    batch_size = x.shape[0]
+    for i in range(batch_size):
+        x_i = x[i].reshape(-1)
+        t_i = target[i].reshape(-1).float()
+        # 只计算FOV内的像素
+        index = torch.ne(t_i, 255)
+        x_i = x_i[index]
+        t_i = t_i[index]
         inter = torch.dot(x_i, t_i)
         sets_sum = torch.sum(x_i) + torch.sum(t_i)
         if sets_sum == 0:
