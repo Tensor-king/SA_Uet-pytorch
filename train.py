c4fddc1a3788bc92fb2f4f3a072f0830b748e810
Theo <zyf1787594682@gmail.com>
2023-07-03 10:06:15 +0800

    芜湖再次起飞

--- train.py
+++ train.py
@@ -38,7 +38,7 @@ class SegmentationPresetTrain:
 class SegmentationPresetEval:
     def __init__(self, crop_size, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)):
         self.transforms = T.Compose([
-#             T.RandomCrop(crop_size),
+            # T.RandomCrop(crop_size),
             T.ToTensor(),
             T.Normalize(mean=mean, std=std),
         ])
